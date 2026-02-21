72bb248737caaff2310b0ec7215f744ed560fced
markmris <mark.mrishih@gmail.com>
2026-02-21 14:49:16 -0800

    Fixed compiler error and added game load from main menu

--- Assets/Scripts/CardReader.cs
+++ Assets/Scripts/CardReader.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+
 public class CardReader : MonoBehaviour
 {
     private string currentCardData;
@@ -12,19 +14,24 @@ public class CardReader : MonoBehaviour
     {
         public string name;
         public string hexColor;
-        public float _color1;
-        public float _color2;
-        public float _color3;
-        public int _hatIndex;
+        public float color1;
+        public float color2;
+        public float color3;
+        public int hatIndex;
 
         public CardPlayerData(string _name, float _color1, float _color2, float _color3, int _hatIndex)
         {
             name = _name;
-            _color1 = _color1;
-            _color2 = _color2;
-            _color3 = _color3;
-            _hatIndex = _hatIndex;
+            color1 = _color1;
+            color2 = _color2;
+            color3 = _color3;
+            hatIndex = _hatIndex;
         }
+    }
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
     }
 
     void Update()
@@ -45,12 +52,14 @@ public class CardReader : MonoBehaviour
     private void HandleCard()
     {
         Debug.Log("CARD SCANNED" + currentCardData);
+        Scene currentScene = SceneManager.GetActiveScene();
         
-        if (cardDatabase.ContainsKey(currentCardData))
+        if (cardDatabase.ContainsKey(currentCardData) && currentScene.name == "Main Menu")
         {
-            Debug.Log("CARD EXISTS IN DATABASE");
+            Debug.Log("CARD EXISTS IN DATABASE. LOADING GAME SCENE");
+            SceneManager.LoadScene("Game");
         }
-        else if(playersIndex<players.Length)
+        else if (playersIndex < players.Length && currentScene.name == "Main Menu")
         {
             Debug.Log("CARD NOT FOUND IN DATABASE. ADDING");
             CardPlayerData temp = new CardPlayerData("NAME HERE", Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0, hatListLength));
